25489a7ff073267a8be4c66394add7f7a5decf6a
Guido Bonomini <bono_9475@hotmail.com>
2020-06-10 15:08:35 -0300

    Services.py & utils.py comments for Products

--- rtmeli/products/services.py
+++ rtmeli/products/services.py
@@ -5,10 +5,12 @@ from products.utils import find_most_sold, process_threads, rearrange_dictionary
 from api.services import MercadolibreApi
 
 def get_category_items_count(category_id):
+    #gets the total items in a category
     meli = MercadolibreApi(category=category_id)
     return meli.get_category()['total_items_in_this_category']
 
 def get_top_items_by_price(category_id, limit=50, sort=''):
+    #gets the top priced items in a category
     meli = MercadolibreApi(category=category_id, limit=limit, sort=sort)
     items = meli.get_items_by_category()
     return [
@@ -20,6 +22,7 @@ def get_top_items_by_price(category_id, limit=50, sort=''):
         for item in items]
 
 def get_most_sold_seller_ids(category_id, total_items, limit=50, offset=0):
+    #gets the best sellers ids in a category
     nthreads = total_items // limit + (1 if (total_items % limit > 0) else 0)
     items = process_threads(
         process_request, category=category_id, 
@@ -31,18 +34,23 @@ def get_most_sold_seller_ids(category_id, total_items, limit=50, offset=0):
     return sellers_ids
 
 def get_user_nickname(users):
+    #gets the users nicknames by id
     meli = MercadolibreApi()
     items = process_threads_users(
         process_request_users, users=users
     )
     return items
 
+# TODO: Unify both methods since they do similar tasks
+
 def process_request(category, limit, offset, items):
+    #process Meli api calls and adds the results to an array
     meli = MercadolibreApi(category=category, limit=limit, offset=offset)
     items.extend(meli.get_items_by_category())
     return items
 
 def process_request_users(user, items):
+    #process Meli api calls and assigns the results to an array
     meli = MercadolibreApi()
     items[user['position']] = (meli.get_user_information(user['id']))
     return items
--- rtmeli/products/utils.py
+++ rtmeli/products/utils.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from threading import Thread
 
 def find_most_sold(items, range):
+    #adds the sold_quantity for a users sold items and sorts it by top sellers
     most_sold = defaultdict(int)
     for item in items:
         most_sold[item['seller']['id']] += item['sold_quantity']
@@ -9,9 +10,12 @@ def find_most_sold(items, range):
     return most_sold[:range]
 
 def rearrange_dictionary(items):
+    #creates a list of dictionaries for the sellers
     return [{'id': item[0],'position': idx} for idx, item in enumerate(items)]
 
+## TODO: Unify process_threads and process_threads_users since they perform a similar task
 def process_threads(function, **params):
+    #process parallel threads for calling Mercadolibre api without mayor delay
     items = []
     threads = []
     for i in range(params.get('nthreads',1)):
@@ -29,6 +33,7 @@ def process_threads(function, **params):
     return items
 
 def process_threads_users(function, users):
+    #process parallel threads for calling Mercadolibre api without mayor delay
     items = [0]*len(users)
     threads = []
     for i in range(len(users)):

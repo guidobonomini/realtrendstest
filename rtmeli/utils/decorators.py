fef83ce39dbc737de33db98d4a1335403616dfa9
Guido Bonomini <bono_9475@hotmail.com>
2020-06-10 16:55:51 -0300

    Add 403 to decorator

--- rtmeli/utils/decorators.py
+++ rtmeli/utils/decorators.py
@@ -1,12 +1,12 @@
 from django.core.exceptions import ObjectDoesNotExist
-
+from django.http import HttpResponseRedirect
 
 def login_required():
     def _dec(view_func):
         def _wrapped_view(request, *args, **kwargs):      
             token = request.session.get('access_token', None)
             if not token:
-                return HttpResponse('Unauthorized', status=401)
+                return render(request, '403.html')
             return view_func(request, *args, **kwargs)
         return _wrapped_view
     return _dec

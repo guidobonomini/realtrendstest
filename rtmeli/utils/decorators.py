4aed0b82aa245ca7a546e0355a9037baf79033ce
Guido Bonomini <bono_9475@hotmail.com>
2020-06-10 16:47:00 -0300

    Remove logger in decorator

--- rtmeli/utils/decorators.py
+++ rtmeli/utils/decorators.py
@@ -1,15 +1,11 @@
-import logging
-
 from django.core.exceptions import ObjectDoesNotExist
 
-logger = logging.getLogger('django')
 
 def login_required():
     def _dec(view_func):
         def _wrapped_view(request, *args, **kwargs):      
             token = request.session.get('access_token', None)
             if not token:
-                logger.error('Token not in session', exc_info=True, extra={'request': request.data})
                 return HttpResponse('Unauthorized', status=401)
             return view_func(request, *args, **kwargs)
         return _wrapped_view
